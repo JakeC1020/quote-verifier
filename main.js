9200d2fbb703f118fe6a9c7353c631470c79c5ad
Jake Crawford <jake.crawford@outlook.com>
2015-10-31 17:14:12 -0600

    Add skeleton for api i/o

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
+// Vender modules
 var express = require('express');
 var fs = require('fs');
 var request = require('request');
+// Personal modules
+var scraper = require('lib/scraper.js');
+
 var app = express();
 
 app.get('/api', function (req, res) {
 	// get url query vars and pass to scraper
-	
+	var queryVars = req.query;
+	var cleanedQueryVars = scraper.manageInput(queryVars);
+	// Send results back to user
+	res.send(scraper.getOutput());
 	
 });
 
