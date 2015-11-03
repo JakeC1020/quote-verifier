630767f6b2ae09efe2aaced9758a01c48d64ade9
Jake Crawford <jake.crawford@outlook.com>
2015-11-03 11:21:47 -0700

    Redo skeleton for async approach

--- main.js
+++ main.js
@@ -2,18 +2,41 @@
 var express = require('express');
 var fs = require('fs');
 var request = require('request');
+var cheerio = require('cheerio');
+var async = require('async');
 // Personal modules
-var scraper = require('lib/scraper.js');
+var scraper = require('./lib/scraper');
 
 var app = express();
 
 app.get('/api', function (req, res) {
 	// get url query vars and pass to scraper
-	var queryVars = req.query;
-	var cleanedQueryVars = scraper.manageInput(queryVars);
-	// Send results back to user
-	res.send(scraper.getOutput());
 	
+	async.waterfall([
+		function (callback) {
+			var queryVars = req.query;
+			if (false) {
+				// query vars not specified correctly
+				callback(true, queryVars);
+			}
+			else {
+				// query vars specified correctly  
+				callback(null, queryVars);
+			}
+		},
+		scraper.manageInput, // Passes cleanName, cleanQuote to callback
+		scraper.scrape, // Passes cleanName, cleanQuote, [quoteArray] to callback
+		scraper.calcAuthenticity, // Passes JSON string to callback
+		
+		], 
+		function (error, jsonResults) { // Function to call at end of waterfall
+		if (error) {
+			// Send default error JSON suggesting user lookup quote on own
+		}
+		else {
+			res.send(result);
+		}
+	});	
 });
 
 app.listen('8081');
